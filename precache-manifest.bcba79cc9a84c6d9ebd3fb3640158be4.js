ef05f8213bad66bcaaa1f3d6c1553045ec9d5b0d
Tai Bui <vantaibui92@gmail.com>
2020-07-26 23:15:24 +0700

    Updates

---
+++ precache-manifest.bcba79cc9a84c6d9ebd3fb3640158be4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a5f2fac37eda886aca487b33581d8c",
+    "url": "/Glasses/index.html"
+  },
+  {
+    "revision": "f828c0bc19e270ae8b19",
+    "url": "/Glasses/static/css/main.d10cc240.chunk.css"
+  },
+  {
+    "revision": "067c4c096c8ba54009c3",
+    "url": "/Glasses/static/js/2.e0d52132.chunk.js"
+  },
+  {
+    "revision": "a56aa9951b0e669f6f38adb273f204e1",
+    "url": "/Glasses/static/js/2.e0d52132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f828c0bc19e270ae8b19",
+    "url": "/Glasses/static/js/main.b131b71a.chunk.js"
+  },
+  {
+    "revision": "26d7db241bc15b6e0209",
+    "url": "/Glasses/static/js/runtime-main.fe7d87b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Glasses/static/media/logo.5d5d9eef.svg"
+  }
+]);
